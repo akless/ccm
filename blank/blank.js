5975717d4cac64eddd28b75a23039083ddae2b6e
Andre Kless <andre.kless@inf.h-brs.de>
2016-05-03 09:43:32 +0200

    minor change

--- blank/blank.js
+++ blank/blank.js
@@ -3,21 +3,21 @@
  * @author André Kless <andre.kless@web.de> 2016
  */
 ccm.component( {
-  
+
   name: 'blank',
 
   Instance: function () {
-    
+
     this.render = function ( callback ) {
-      
+
       var element = ccm.helper.element( this );
-      
+
       element.html( 'Hello, World!' );
-      
+
       if ( callback ) callback();
-      
+
     }
-    
+
   }
-  
+
 } );
