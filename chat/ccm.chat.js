29a7091aac0f2b5ecf295dc42d532e5d0a80e3f6
Andre Kless <andre.kless@inf.h-brs.de>
2016-05-18 18:42:11 +0200

    follow new component filename convention

--- chat/ccm.chat.js
+++ chat/ccm.chat.js
@@ -8,10 +8,10 @@ ccm.component( {
 
   config: {
 
-    html:  [ ccm.store, { local: 'templates.json' } ],
+    html:  [ ccm.store, { local: 'http://akless.github.io/ccm-developer/resources/ccm.chat.templates.json' } ],
     key:   'test',
     store: [ ccm.store, { url: 'ws://ccm2.inf.h-brs.de/index.js', store: 'chat' } ],
-    style: [ ccm.load, 'style.css' ],
+    style: [ ccm.load, 'http://akless.github.io/ccm-developer/resources/ccm.chat.style.css' ],
     user:  [ ccm.instance, 'https://kaul.inf.h-brs.de/ccm/components/user.js' ]
 
   },
