b1e90505eee837a3f856290df255cff4677ef5bf
Andre Kless <andre.kless@inf.h-brs.de>
2017-08-21 10:14:04 +0200

    ccm v9.3.0 - allow attributes for script and link tags created on ccm.load calls (now subresource integrity is possible)

--- version/ccm-9.3.0.js
+++ version/ccm-9.3.0.js
@@ -2,7 +2,7 @@
  * @overview <i>ccm</i> framework
  * @author André Kless <andre.kless@web.de> 2014-2017
  * @license The MIT License (MIT)
- * @version latest (9.3.0)
+ * @version 9.3.0
  * @changes
  * version 9.3.0 (21.08.2017):
  * - allow attributes for script and link tags created on ccm.load calls (now subresource integrity is possible)
