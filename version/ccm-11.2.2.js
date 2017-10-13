67266aee565542ba5db2e769ff1ec4d9c801db16
Andre Kless <andre.kless@inf.h-brs.de>
2017-10-13 10:29:58 +0200

    ccm v11.2.2: bugfix for embedding ccm instance in the same HTML element

--- version/ccm-11.2.2.js
+++ version/ccm-11.2.2.js
@@ -2,7 +2,7 @@
  * @overview <i>ccm</i> framework
  * @author André Kless <andre.kless@web.de> 2014-2017
  * @license The MIT License (MIT)
- * @version latest (11.2.2)
+ * @version 11.2.2
  * @changes
  * version 11.2.2 (13.10.2017): bugfix for embedding ccm instance in the same HTML element
  * version 11.2.1 (13.10.2017): bugfix for ccm.helper.solveDependency()
