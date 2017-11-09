eab6ce8321a3693aaa5aee27d7fd1458a7821ec1
Andre Kless <andre.kless@inf.h-brs.de>
2017-11-09 22:53:05 +0100

    ccm v12.3.1: update 'ccm.helper.encodeDependencies(obj)' and 'ccm.helper.decodeDependencies(obj)': respect deeper properties

--- version/ccm-12.3.1.js
+++ version/ccm-12.3.1.js
@@ -2,7 +2,7 @@
  * @overview <i>ccm</i> framework
  * @author André Kless <andre.kless@web.de> 2014-2017
  * @license The MIT License (MIT)
- * @version latest (12.3.1)
+ * @version 12.3.1
  * @changes
  * version 12.3.1 (09.11.2017):
  * - update 'ccm.helper.encodeDependencies(obj)' and 'ccm.helper.decodeDependencies(obj)': respect deeper properties
