6720276682796f7f1ed5b34822db811f0ac29036
Andre Kless <andre.kless@inf.h-brs.de>
2018-03-06 12:11:39 +0100

    ccm v16.0.1: update ccm.helper.dataset, ccm.helper.isDataset, ccm.helper.isDatastore and ccm.helper.isKey

--- version/beta/ccm-16.0.1.js
+++ version/beta/ccm-16.0.1.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2018
  * @license The MIT License (MIT)
- * @version latest (16.0.1)
+ * @version 16.0.1
  * @changes
  * version 16.0.1 (06.03.2018)
  * - update ccm.helper.dataset
