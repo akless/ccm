25a88349226047641052e0f0dbee5e02f472b32a
akless <andre.kless@web.de>
2018-04-14 10:41:50 +0200

    ccm v16.1.0: ccm.helper.formData with integrated escaping of HTML characters

--- version/beta/ccm-16.1.0.js
+++ version/beta/ccm-16.1.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2018
  * @license The MIT License (MIT)
- * @version latest (16.1.0)
+ * @version 16.1.0
  * @changes
  * version 16.1.0 (14.04.2018): ccm.helper.formData with integrated escaping of HTML characters
  * version 16.0.1 (12.04.2018): bugfix in ccm.helper.dataset
